bb2d3d88bc4a77b0698513cc23ec3e23ecb9604f
孟卫平 <mwping1324@163.com>
2021-03-24 20:16:24 +0800

    build: Publish to Github Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,14 @@ buildscript {
         google()
         mavenCentral()
         jcenter()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/AtlasXV/android-libs")
+            credentials {
+                username = System.getenv("GPR_USR") ?: project.findProperty("GPR_USR").toString()
+                password = System.getenv("GPR_KEY") ?: project.findProperty("GPR_KEY").toString()
+            }
+        }
     }
 
     dependencies {
@@ -15,6 +23,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.30") // publisher uses 1.4.20 which goes OOM
         classpath("io.deepmedia.tools:publisher:0.5.0")
+        classpath("com.atlasv.android.publishlib:plugin:1.2.2")
     }
 }
 
@@ -23,6 +32,14 @@ allprojects {
         google()
         mavenCentral()
         jcenter()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/AtlasXV/android-libs")
+            credentials {
+                username = System.getenv("GPR_USR") ?: project.findProperty("GPR_USR").toString()
+                password = System.getenv("GPR_KEY") ?: project.findProperty("GPR_KEY").toString()
+            }
+        }
     }
 }
 
