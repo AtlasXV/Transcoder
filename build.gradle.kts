ce3035cfae091963af23d9dc83dc81e309fc8eae
孟卫平 <mwping1324@163.com>
2021-03-28 09:07:47 +0800

    build: Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
-        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("com.android.tools.build:gradle:4.1.3")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.30") // publisher uses 1.4.20 which goes OOM
         classpath("io.deepmedia.tools:publisher:0.5.0")
         classpath("com.atlasv.android.publishlib:plugin:1.2.2")
