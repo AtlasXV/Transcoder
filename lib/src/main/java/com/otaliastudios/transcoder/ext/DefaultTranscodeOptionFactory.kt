697e2cc5a47a4b86b16b161c04a1dfc3212dad6d
孟卫平 <mwping1324@163.com>
2021-04-21 20:19:23 +0800

    chore: Fix build error

--- lib/src/main/java/com/otaliastudios/transcoder/ext/DefaultTranscodeOptionFactory.kt
+++ lib/src/main/java/com/otaliastudios/transcoder/ext/DefaultTranscodeOptionFactory.kt
@@ -7,7 +7,7 @@ import com.otaliastudios.transcoder.TranscoderOptions
  * 3/25/21
  */
 class DefaultTranscodeOptionFactory(private val transcoderOptions: TranscoderOptions) : TranscodeOptionFactory {
-    override fun create(): TranscoderOptions {
+    override fun create(currentRetryTimes: Int): TranscoderOptions {
         return transcoderOptions
     }
 
